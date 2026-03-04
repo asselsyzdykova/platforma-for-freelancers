ffd978aea45f86a631cec4a9ed79b51e5094a709
Assel Syzdykova <syzdykova_2024@mail.ru>
2026-03-04 17:10:58 +0100

    cors3

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -12,13 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-        'https://platforma-for-freelancers.vercel.app'],
+    'allowed_origins' => ['https://platforma-for-freelancers.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
